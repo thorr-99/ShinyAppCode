f3b0f32a9f7ba59b0ef5ce23491e34b6a52d797b
thorr-99 <61893010+thorr-99@users.noreply.github.com>
2020-05-09 16:07:52 -0400

    Retirement Calculator Codes
    New App code for https://thorr-99.shinyapps.io/Retirement_Calculator/

--- server.R
+++ server.R
@@ -1,40 +1,112 @@
 library(shiny)
-
-library(ggplot2)
+library(quantmod)
+library(plotly)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
     
-    data <- reactive({
-        if (input$distribution == "Uniform") {
-            x <- runif(input$Size)
-        } else if (input$distribution == "Normal") {
-            x <- rnorm(input$Size)
-        } else if (input$distribution == "Exponential") {
-            x <- rexp(input$Size)
-        } else {
-            x <-rbinom(10, input$Size, prob = 0.1)
+    #1f77b4 or rgb(31, 119, 180)  // muted blue
+    #ff7f0e or rgb(255, 127, 14)  // safety orange
+    #2ca02c or rgb(44, 160, 44)   // cooked asparagus green
+    #d62728 or rgb(214, 39, 40)   // brick red
+    #9467bd or rgb(148, 103, 189) // muted purple
+    #8c564b or rgb(140, 86, 75)   // chestnut brown
+    
+    mycolors = c('1f77b4', 'ff7f0e', '2ca02c', 'd62728')
+    
+    balwint <- function(v0, anncont, int, term) {
+        bal <- numeric()
+        beg = v0
+        for (i in seq(1:term)) {
+            end <-  (beg + anncont) * (1 + int)
+            bal <- c(bal, end)
+            beg <- end
         }
-    }) 
+        return(bal)
+    }
+    
+    #Annuity Factor for age 65 to 75 based on 2% interest rate:
+    #calculated in worksheet 
+    ax <- data.frame('age' = c(65:75), 
+                     'M' = c( 16.72, 16.18, 15.64, 15.10, 14.56, 14.01,  
+                              13.47, 12.93, 12.39, 11.86, 11.34),  
+                     'F' = c( 17.84, 17.29, 16.75, 16.21, 15.66, 15.12,
+                              14.57, 14.03, 13.49, 12.95, 12.41)) 
+
+    asset <- reactive({
+        v0 <- input$Val0 
+        age <- seq(
+            input$ageRange[1], 
+            input$ageRange[2]
+        )
+        term <- length(age)
+        anncont <- input$contribution * 12
+    
+        zeroint <- balwint(v0, anncont, 0, term)
+        twopct <- balwint(v0, anncont, 0.02, term)
+        fourpct <- balwint(v0, anncont, 0.04, term)
+        sixpct <- balwint(v0, anncont, 0.06, term)
         
-    output$plot1 <- renderPlot({
-        plot(data(), main = paste("Scatter plot of", input$distribution), 
-                ylab = "Simulated Data")
-    })
+        data.frame(age, zeroint, twopct, fourpct, sixpct)
     
-    
-    output$plot2 <- renderPlot({
-        hist(data(), breaks = 20, freq = FALSE, 
-             main = paste("Histogram of", input$distribution), 
-             xlab = "Simulated Data")
-        lines(density(data()), col=2)
-        })
-                    
-
-    output$stats <- renderPrint({
-        summary(data())
     })
 
+    output$assetplot <- renderPlotly({
+        fig <- plot_ly(asset(), x = ~age, y = ~zeroint, type = 'scatter', mode = 'lines', 
+                    name = '0%', fill='tozeroy', color = mycolors[1]) 
+        fig <- fig %>% add_trace(y = ~twopct, type = 'scatter', mode = 'lines', 
+                                  name = '2%', fill='tonexty', color = mycolors[2])
+        fig <- fig %>% add_trace(y = ~fourpct, type = 'scatter', mode = 'lines', 
+                                  name = '4%', fill='tonexty', color = mycolors[3])
+        fig <- fig %>% add_trace(y = ~sixpct, type = 'scatter', mode = 'lines', 
+                                  name = '6%', fill='tonexty', color = mycolors[4])
+        fig <- fig %>% layout(title = "Retirement Asset Growth under Interest Scenarios",
+            yaxis = list (title = 'Asset Growth'))
+
+    })
     
+    output$assettbl <- renderTable({
+        
+        tail(asset(),1)
+    })
+    
+
+    output$retireplot <- renderPlotly({
+        #base plot
+        #ret.asset = as.matrix(tail(asset(), 1))
+        #barplot(ret.asset[1, -1], col=rainbow(10), names.arg = c('0%', '2%', '4%', '6%'), 
+        #        main = "Projected Retirement Assets")
+        ret.asset = tail(asset(), 1)
+        plot_ly(x = c('0%', '2%', '4%', '6%'), 
+                y = as.numeric(ret.asset[1,-1]), 
+                type = 'bar', color = mycolors, 
+                text = paste0('$', round(ret.asset[1, -1]/1000,0), 'K')) %>%
+            add_text(textfont = list(color='black'), 
+                     textposition = 'bottom') %>% 
+            layout(title = "Projected Retirement Assets", 
+                   xaxis = list(title = "Earned Interest Rate"),
+                   showlegend = FALSE
+            )
+ 
+    })
+    
+    output$incomeplot <- renderPlotly({
+        ret.asset <- tail(asset(), 1)
+        annfactor <- ax[ax$age == input$ageRange[2], ifelse(input$sex== "Male", 2, 3)] 
+        retIncome <- as.numeric(ret.asset[1, -1]) / annfactor / 12
+        
+        plot_ly(y = factor(c('6%', '4%', '2%', '0%'), levels = c('6%', '4%', '2%', '0%')),
+                x = rev(retIncome), 
+                type = 'bar',
+                orientation = 'h', 
+                color = rev(mycolors), 
+                text = paste0('$', round(rev(retIncome),0))) %>%
+            add_text(textfont = list(color = 'black'), 
+                     textposition = 'left') %>%
+            layout(title = "Monthly Retirement Income", 
+                   yaxis = list(title = "Earned Rate Scenario"),
+                   showlegend = FALSE
+            )
+        
+    })
 })
-
--- ui.R
+++ ui.R
@@ -1,39 +1,47 @@
 library(shiny)
+library(plotly)
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Common Statistical Distributions"),
+    titlePanel(" "),
 
-    # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            selectInput("distribution", "Select a Distribution", 
-                        list("Uniform", "Normal", "Exponential", "Binomial")),
-            sliderInput("Size",
-                        "Sample Size:",
-                        min = 100,
-                        max = 5000,
-                        value = 400),
-            submitButton("submit")
+            h2("Retirement Asset: "),
+            numericInput("Val0", h3("Current Retirement Savings:"), 
+                         value = 20000, step = 1000),
+            sliderInput("ageRange", h3("Starting Age and Retirement Age: "), 
+                        min = 20, max = 75, value = c(40, 65)),
+            sliderInput(inputId = "contribution",
+                        h3("Monthly Contribution: "), min = 50, max = 2000, step = 50,
+                        value = 100),
+            radioButtons("sex", h3("Gender: "), choices=list('Male', 'Female')),
+            submitButton("Refresh")
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
+            h1("How Much Will You Have at Retirement?", 
+               style = 'color: blue'),
+            htmlOutput('br'),
             fluidRow(
-                column(width = 5, 
-                    plotOutput("plot1", click = "plot_click")
-                ),
+                plotlyOutput('assetplot'),
                 
-                column(width = 5, 
-                    plotOutput("plot2")
-                )
+            ), 
+            fluidRow(
+                h3("Retirement Asset and Income at Retirement Age: "),
+                #tableOutput("assettbl")
             ),
-            h4("Summary Statistics: "),
-            verbatimTextOutput("stats"),
-        
-
+            fluidRow(
+                column(width=5,
+                       #h4("asset summary"),
+                       plotlyOutput('retireplot')), 
+                column(width = 5, 
+                       #h4("income comparison"),
+                       plotlyOutput('incomeplot'))
+            )
+            
         )
     )
 ))
